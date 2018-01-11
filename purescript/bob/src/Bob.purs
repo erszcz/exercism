3eacc6b1bbec19d08295c673901d136a6e62762c
Radek Szymczyszyn <lavrin@gmail.com>
2018-01-11 21:35:48 +0100

    Task #3: Bob

--- purescript/bob/src/Bob.purs
+++ purescript/bob/src/Bob.purs
@@ -3,7 +3,8 @@ module Bob
   ) where
 
 import Prelude
-import Data.Array (last)
+import Data.Array (foldl, last)
+import Data.Char.Unicode
 import Data.String (toCharArray)
 import Data.Maybe ( Maybe(..) )
 
@@ -22,9 +23,27 @@ hey :: String -> String
 hey sentence = show $ reactionTo sentence
 
 reactionTo :: String -> Reaction
-reactionTo sentence =
-  case last $ toCharArray sentence of
-    Nothing  -> Fine
-    Just '?' -> Sure
-    Just '!' -> ChillOut
-    Just _   -> Whatever
+reactionTo s
+  | isSilence s  = Fine
+  | isYell s     = ChillOut
+  | isQuestion s = Sure
+  | otherwise    = Whatever
+
+isSilence s = foldl isSpace true $ toCharArray s
+  where isSpace acc c = case generalCategory c of
+          Just Space -> acc
+          Just Control -> acc
+          _ -> false
+
+isQuestion s
+  | last (toCharArray s) == Just '?' = true
+  | otherwise = false
+
+isYell s =
+  case foldl step { lower: 0, upper: 0 } $ toCharArray s of
+    { lower: 0, upper: n } | n > 0 -> true
+    _ -> false
+  where step { lower: l, upper: u } c = case generalCategory c of
+          Just LowercaseLetter -> { lower: l + 1, upper: u }
+          Just UppercaseLetter -> { lower: l,     upper: u + 1 }
+          _                    -> { lower: l,     upper: u }
