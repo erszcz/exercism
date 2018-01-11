0640b53ef00854490b2ddfd224fa37cd786e8ad8
Radek Szymczyszyn <lavrin@gmail.com>
2018-01-11 23:59:25 +0100

    Prettify task #4 a bit

--- purescript/raindrops/src/Raindrops.purs
+++ purescript/raindrops/src/Raindrops.purs
@@ -3,18 +3,21 @@ module Raindrops
   ) where
 
 import Prelude
-import Data.String (joinWith)
+import Data.Array (fold, mapMaybe)
+import Data.Maybe (Maybe(..))
 
 raindrops :: Int -> String
 raindrops n = case factors n of
     [] -> show n
-    fs -> joinWith "" $ map repr fs
+    fs -> fold $ mapMaybe repr fs
   where
     repr i = case i of
-      3 -> "Pling"
-      5 -> "Plang"
-      _ -> "Plong"
+      3 -> Just "Pling"
+      5 -> Just "Plang"
+      7 -> Just "Plong"
+      _ -> Nothing
 
+factors :: Int -> Array Int
 factors n =
   (if n `mod` 3 == 0 then [3] else []) <>
   (if n `mod` 5 == 0 then [5] else []) <>
