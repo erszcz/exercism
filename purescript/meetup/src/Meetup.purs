0ce6131552bb8bbfcd9a86eb80cffb4e0d93f650
Radek Szymczyszyn <lavrin@gmail.com>
2018-01-13 17:21:37 +0100

    wip: Task #12

--- purescript/meetup/src/Meetup.purs
+++ purescript/meetup/src/Meetup.purs
@@ -1,13 +1,18 @@
 module Meetup
-  ( meetup
-  , Week(..)
-  ) where
+  {--( meetup--}
+  {--, Week(..)--}
+  {--) --}
+  where
 
-import Prelude
 import Data.Date
-import Data.Enum (fromEnum, toEnum)
-import Data.Maybe (Maybe(..))
 import Partial.Unsafe
+import Prelude
+
+import Control.Apply (lift2)
+import Data.Array as A
+import Data.Enum (fromEnum, toEnum)
+import Data.Foldable as F
+import Data.Maybe (Maybe(..))
 
 data Week = First
           | Second
@@ -16,19 +21,28 @@ data Week = First
           | Last
           | Teenth
 
-{--meetup :: Year -> Month -> Week -> Weekday -> Maybe Date--}
-{--meetup year month week day = Just $ unsafePartial meetup' (toEnum 2013) (toEnum 5) (toEnum 13)--}
-{--  where meetup' :: Partial => Maybe Year -> Maybe Month -> Maybe Day -> Date--}
-{--        meetup' (Just y) (Just m) (Just d) = canonicalDate y m d--}
-
 meetup :: Year -> Month -> Week -> Weekday -> Maybe Date
-meetup year month week day = canonicalDate year month <$> dayteenth week day
-  where dayteenth w d = toEnum $ (weekOffset w) + (fromEnum d)
-        weekOffset w = case w of
-                            First -> 0
-                            Second -> 7
-                            Third -> 14
-                            Fourth -> 21
-                            Last -> 28
-                            Teenth -> 12
-                            Teenth -> 12
+meetup year month week day = unsafePartial meetup' (toEnum 2013) (toEnum 5) (toEnum 13)
+  where meetup' :: Partial => Maybe Year -> Maybe Month -> Maybe Day -> Maybe Date
+        meetup' (Just y) (Just m) (Just d) =
+          canonicalDate y m <$> toEnum (dayteenth Monday week d)
+
+{--meetup :: Year -> Month -> Week -> Weekday -> Maybe Date--}
+{--meetup year month week day = canonicalDate year month <$> dayteenth (firstWeekday year month) week day--}
+
+dayteenth :: Weekday -> Week -> Day -> Int
+{--dayteenth i w d = (+) <$> (fromEnum <$> i) <@> (fromEnum d)--}
+dayteenth monthFirst w d = F.sum [fromEnum monthFirst, weekOffset w, fromEnum d]
+
+  {--<@> (weekOffset w)--}
+
+firstWeekday year month = weekday <$> canonicalDate year month <$> (toEnum 1)
+
+weekOffset w =
+  case w of
+       First -> 0
+       Second -> 7
+       Third -> 14
+       Fourth -> 21
+       Last -> 28
+       Teenth -> 12
