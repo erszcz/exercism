4a14652c9f8f6e9587dd0fc42332230d8d9e61eb
Radek Szymczyszyn <lavrin@gmail.com>
2018-01-16 23:45:23 +0100

    Task #12: Meetup - ripped off of http://exercism.io/dbalmain

--- purescript/meetup/src/Meetup.purs
+++ purescript/meetup/src/Meetup.purs
@@ -1,86 +1,28 @@
 module Meetup
-  {--( meetup--}
-  {--, Week(..)--}
-  {--) --}
+  ( meetup
+  , Week(..)
+  )
   where
 
 import Prelude
 
-import Control.Apply (lift3)
+import Data.Array ((..))
 import Data.Array as A
 import Data.Date
-import Data.Enum (succ, toEnum)
-import Data.Generic (class Generic, gShow)
-import Data.Maybe (Maybe(..))
-import Data.Traversable as T
-import Partial.Unsafe
+import Data.Enum (fromEnum, toEnum)
+import Data.Maybe (Maybe)
 
 data Week = First | Second | Third | Fourth | Last | Teenth
 
-data DayInfo = DayInfo
-  { no :: Int,
-    week :: Int,
-    weekday :: Maybe Weekday,
-    teenth :: Boolean
-  }
-
 meetup :: Year -> Month -> Week -> Weekday -> Maybe Date
-meetup year month week weekday =
-  lift3 canonicalDate (pure year) (pure month) (join $ toEnum <$> maybeDay)
+meetup y m w wd =
+  A.find (\cd -> weekday cd == wd) $ canonicalDate y m <$> A.catMaybes (toEnum <$> days)
   where
-    days = eligibleDays year month week weekday
-    maybeDay = dayInfoNo <$> (A.last days)
-
-dayInfoNo :: DayInfo → Int
-dayInfoNo (DayInfo { no }) = no
-
-derive instance genericTriangle :: Generic DayInfo
-instance showDayInfo :: Show DayInfo where
-  show = gShow 
-
-dayInfoFromWeekday :: Weekday → DayInfo
-dayInfoFromWeekday weekday =
-  DayInfo { no: 1, week: 1, weekday: Just weekday, teenth: false }
-
-dayInfo :: Int → Int → Maybe Weekday → Boolean → DayInfo
-dayInfo no week weekday teenth =
-  DayInfo { no: no, week: week, weekday: weekday, teenth: teenth }
-
-next :: DayInfo -> DayInfo
-next (DayInfo { no, week, weekday, teenth }) =
-  dayInfo (no + 1)
-          (no `div` 7 + 1)
-          (case succ weekday of
-             Nothing -> Just Monday
-             Just wd -> wd)  
-          (if no >= 12 && no < 19
-              then true
-              else false)
-
-eligibleDays :: Year → Month → Week → Weekday → Array DayInfo
-eligibleDays year month week weekday =
-  A.filter filterDay mInfo
-  where
-    mFirstWeekday = monthFirstWeekday year month
-    mInfo = monthDaysInfo mFirstWeekday
-    filterDay (DayInfo { no: no, week: wk, weekday: wd, teenth: teenth }) =
-      case week of
-        First  -> wk == 1 && wd == (pure weekday)
-        Second -> wk == 2 && wd == (pure weekday)
-        Third  -> wk == 3 && wd == (pure weekday)
-        Fourth -> wk == 4 && wd == (pure weekday)
-        Last   -> wd == (pure weekday) 
-                  && ( ((isLeapYear year || month /= February) && wk == 5)
-                       || wk == 4 )
-        Teenth -> teenth  && wd == (pure weekday)
-
-monthDaysInfo :: Weekday → Array DayInfo
-monthDaysInfo mFirstWeekday =
-  let initial = dayInfoFromWeekday mFirstWeekday
-   in A.cons initial $ T.scanl (\di _ -> next di) initial (A.range 1 30)
-
-monthFirstWeekday :: Year -> Month -> Weekday
-monthFirstWeekday year month = weekday $ canonicalDate year month $ unsafePartial firstDay
-  where firstDay :: Partial => Day
-        firstDay = case toEnum 1 of
-                        Just day -> day
+    daysInMonth = fromEnum $ lastDayOfMonth y m
+    days = case w of
+                First  -> 1  .. 7
+                Second -> 8  .. 14
+                Third  -> 15 .. 21
+                Fourth -> 22 .. 28
+                Teenth -> 13 .. 19
+                Last   -> (daysInMonth - 6) .. daysInMonth
