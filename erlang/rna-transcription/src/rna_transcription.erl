ab0ccdc322c1d1d7d64c0919591136e31da5bd70
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2021-03-23 20:19:54 +0100

    Erlang #3: RNA Transcription - solution

--- erlang/rna-transcription/src/rna_transcription.erl
+++ erlang/rna-transcription/src/rna_transcription.erl
@@ -2,5 +2,10 @@
 
 -export([to_rna/1]).
 
+to_rna([]) -> [];
+to_rna([H | T]) -> [dna_to_rna(H) | to_rna(T)].
 
-to_rna(_Strand) -> undefined.
+dna_to_rna($G) -> $C;
+dna_to_rna($C) -> $G;
+dna_to_rna($T) -> $A;
+dna_to_rna($A) -> $U.
